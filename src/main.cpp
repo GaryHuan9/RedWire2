19f7301de0d39e0c167976934f48f60865856031
Gary Huang <henhaodegaigai@gmail.com>
2022-11-04 22:01:06 -0400

    Fully initialized project

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,20 @@
 
 int main()
 {
-	// create the window
-	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "My window");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Red Wire 2");
 
-	// run the program as long as the window is open
 	while (window.isOpen())
 	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
+		sf::Event event{};
 		while (window.pollEvent(event))
 		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				window.close();
+			if (event.type == sf::Event::Closed) window.close();
 		}
 
-		// clear the window with black color
 		window.clear(sf::Color::Black);
 
-		// draw everything here...
-		// window.draw(...);
 
-		// end the current frame
+
 		window.display();
 	}
 
